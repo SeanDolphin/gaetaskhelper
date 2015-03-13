cae3ff7e8f55b1252e96f3dc7fcea55107e6e03a
Sean Dolphin <Sean.Dolphin@kpcompass.com>
2015-03-13 09:40:42 -0700

    updated to point to google.golang.org/appengine instead of appengine

--- module.go
+++ module.go
@@ -1,22 +1,23 @@
 package gaetaskhelper
 
 import (
-	"appengine"
-	"appengine/module"
-	"appengine/taskqueue"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/module"
+	"google.golang.org/appengine/taskqueue"
 )
 
 var defaultLevels map[string]string = map[string]string{}
 
-func SetModule(context appengine.Context, task *taskqueue.Task, moduleName string) error {
+func SetModule(ctx context.Context, task *taskqueue.Task, moduleName string) error {
 	hostName, ok := defaultLevels[moduleName]
 	if !ok {
-		version, err := module.DefaultVersion(context, moduleName)
+		version, err := module.DefaultVersion(ctx, moduleName)
 		if err != nil {
 			return err
 		}
 
-		hostName, err = appengine.ModuleHostname(context, moduleName, version, "")
+		hostName, err = appengine.ModuleHostname(ctx, moduleName, version, "")
 		if err != nil {
 			return err
 		}
